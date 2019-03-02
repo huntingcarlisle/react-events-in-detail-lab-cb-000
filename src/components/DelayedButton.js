59632c56301cba039a1b2e2ff48c37256256858b
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-03-02 02:34:51 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,9 @@
 // Code DelayedButton Component Here
+import React from 'react';
+import ReactDOM dfrom 'react-dom';
+
+export default class DelayedButton extends React.Component {
+  render() {
+    return null;
+  }
+}

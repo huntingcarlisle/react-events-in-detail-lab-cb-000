bcb0727f73334b9ec956ff7bba6ed0eb48c8fd91
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-03-02 02:42:22 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class CoordinatesButton extends React.Component {
-  onReceiveCoordinates = (prop) => {
-    return console.log(prop);
+  onReceiveCoordinates = (event) => {
+    return console.log(event.target);
   }
   
   render() {
